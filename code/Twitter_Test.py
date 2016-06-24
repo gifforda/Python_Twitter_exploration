06f993add77fe3ab3ac997073a2d1367b3e12319
Aliya Gifford <aliya.gifford@vanderbilt.edu>
2016-06-24 12:27:31 -0500

    committing previously created code data and jupyter notebooks

---
+++ code/Twitter_Test.py
@@ -0,0 +1,28 @@
+# Import the necessary methods from the tweepy library
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+
+# Variables that contain the user credentials to access Twitter Streaming API
+ckey = '' # Customer key
+csecret = '' # Customer secret key
+atoken = '' # Access token 
+asecret = '' # Access secret token
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+# This handles Twitter authentification and the connection to Twitter Streaming API
+    
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, listener())
+    
+# This line filters Twitter Stream to capture data by the keywords: 'python','learning'
+twitterStream.filter(track=['Zika','virus'])
